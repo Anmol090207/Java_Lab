22fe05a9083d55e5577ecc460dab2cb3a4cc691b
Anmol Prasad <anmolprasad2007@gmail.com>
2025-05-12 20:17:35 +0530

    Create Write a program to demonstrate widening type casting.java

---
+++ Java_Lab/Write a program to demonstrate widening type casting.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    int n=20;
+    System.out.println("The integer value:"+n);
+    double d = n;
+    System.out.println("The double value:"+d);
+  }
+}
