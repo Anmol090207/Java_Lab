462a2a338734930384d12380ef0e177d0cb6b98e
Anmol Prasad <anmolprasad2007@gmail.com>
2025-05-12 20:20:18 +0530

    Create Write a program to demonstrate narrowing type casting .java

---
+++ Java_Lab/Write a program to demonstrate narrowing type casting .java
@@ -0,0 +1,8 @@
+class Main{
+  public static void main(String[] args) {
+    double n = 100.523;
+    System.out.println("The double value:"+n);
+    int i = (int)n;
+    System.out.println("The integer value:"+i);
+  }
+}
